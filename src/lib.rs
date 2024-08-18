2710493cc01bc49a1cdf4d9cd2652e73d69d6e9a
faliah <leogoldf21@gmail.com>
2024-08-18 22:43:20 +0100

    classic frogger game in progress

--- src/lib.rs
+++ src/lib.rs
@@ -105,6 +105,37 @@ pub fn reply(message: &str) -> &str {
 }
 
 
+
+#[derive(Debug)]
+pub struct HighScores{
+        scores: [u32],
+    }
+
+impl HighScores {
+    pub fn new(scores: &[u32]) -> Self {
+        // todo!("Construct a HighScores struct, given the scores: {scores:?}")
+        Self{score: *scores,}
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        todo!("Return all the scores as a slice")
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        todo!("Return the latest (last) score")
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        todo!("Return the highest score")
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        todo!("Return 3 highest scores")
+    }
+}
+
+
+
 #[test]
 fn test_prime_factor(){
     assert_eq!(prime_factors(100), [2, 2, 5, 5]);
